fd3fe191924599f5321c5ba0f275cc737115b926
orjichimaprince <orjichimaprince@gmail.com>
2022-09-28 20:05:55 +0100

    responsive designs

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}", "./index.html", "./components/**/*.{html,js}",
+        './pages/**/*.{html,js}'
+    ],
+    // content: [
+    //     './pages/**/*.{html,js}',
+    //     './components/**/*.{html,js}',
+    // ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
